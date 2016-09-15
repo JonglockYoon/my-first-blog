8c1c7f5bdfb10ea12255d770e320cbf508677b61
JonglockYoon <jerry1455@naver.com>
2016-09-15 23:01:35 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
- url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+	url(r'^$', views.post_list, name='post_list'),
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+	url(r'^post/(?P<pk>[0-9]+)/edit/blog.views.post_list$', views.post_list, name='post_list'),
 ]
-
+#http://127.0.0.1:8000/post/4/edit/blog.views.post_detail
