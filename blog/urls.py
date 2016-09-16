c09d5b9552fa097ed4ca05895a1e6a9ca95fc438
JonglockYoon <jerry1455@naver.com>
2016-09-16 13:03:21 +0900

    added remove post on the site

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
 	url(r'^post/new/$', views.post_new, name='post_new'),
 	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 	url(r'^post/(?P<pk>[0-9]+)/edit/blog.views.post_list$', views.post_list, name='post_list'),
+	url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+	url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+	url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
 ]
 #http://127.0.0.1:8000/post/4/edit/blog.views.post_detail
